6e116ec53fbde842f73ff1fd95efb1ef55304fc5
Simon <mail@scderox.de>
2023-08-29 12:26:25 +0200

    feat: added IncludedInPlan component

--- src/components/IncludedInPlan.js
+++ src/components/IncludedInPlan.js
@@ -81,10 +81,11 @@ export default function IncludedInPlan({data = {}, additionalDetails = {}}) {
                         d="m480-320 160-160-160-160-56 56 64 64H320v80h168l-64 64 56 56Zm0 240q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/>
                 </svg>
             </Link>
-            <Link href="https://scnx.app/glink?page=pricing?showUpgradeModal=true" className="button button--secondary">
-                <Translate id="plans.included.upgrade">Upgrade SCNX Plan</Translate></Link>
+            {!data['STARTER'] && <Link href="https://scnx.app/glink?page=pricing?showUpgradeModal=true"
+                                       className="button button--secondary">
+                <Translate id="plans.included.upgrade">Upgrade SCNX Plan</Translate></Link>}
             <Link to="/docs/scnx/guilds/plans" className="button button--secondary">
-                <Translate id="plans.included.upgrade">Learn more about Pricing on SCNX</Translate></Link>
+                <Translate id="plans.included.learnMoreAboutPricing">Learn more about Pricing on SCNX</Translate></Link>
         </div>
     </div>;
 }
