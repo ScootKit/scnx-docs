a1d084411d9e106ba298eb46067eb654715613d6
Simon <mail@scderox.de>
2023-09-01 22:20:26 +0200

    updated component availability

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,7 +1,11 @@
 import MDXComponents from '@theme-original/MDXComponents';
 import IncludedInPlan from '@site/src/components/IncludedInPlan';
+import Tabs from '@theme/Tabs';
+import TabItem from '@theme/TabItem';
 
 export default {
     ...MDXComponents,
-    IncludedInPlan
+    IncludedInPlan,
+    TabItem,
+    Tabs
 };
