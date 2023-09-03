86bf27e7281ca2c1f94a99866930b8191b4e6c80
Simon <mail@scderox.de>
2023-09-03 21:19:43 +0200

    added node-fetch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,6 +3,7 @@
 
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const fetch = require('node-fetch');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
