3601b7dfba415217bbc9c05b87138d3c2633eb86
Simon <mail@scderox.de>
2026-05-10 22:27:31 +0200

    Add documentation for direct messages in sidebars

--- sidebars.js
+++ sidebars.js
@@ -55,6 +55,10 @@ const sidebars = {
                 {
                     type: 'doc',
                     id: 'discord-ids'
+                },
+                {
+                    type: 'doc',
+                    id: 'direct-messages'
                 }
             ]
         },
